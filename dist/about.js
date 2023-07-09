3e9e98bee68e1d0249c3354c559c7afc1305bceb
elaysoiferman <elay@makersteam.co>
2023-07-09 15:51:20 +0300

    About page commit pnpm dev

--- dist/about.js
+++ dist/about.js
@@ -6539,30 +6539,26 @@
   _getGSAP3() && gsap3.registerPlugin(ScrollTrigger2);
 
   // src/about.ts
-  gsapWithCSS.registerPlugin(SplitText);
   gsapWithCSS.registerPlugin(ScrollTrigger2);
   window.Webflow = window.Webflow || [];
   window.Webflow.push(() => {
-    document.addEventListener("DOMContentLoaded", function() {
-      new SplitText(".about_side-text", {
-        type: "words, chars",
-        tagName: "span"
-      });
-      const tl = gsapWithCSS.timeline({
-        scrollTrigger: {
-          trigger: ".about_side-text-wrapper",
-          scrub: true,
-          pin: true,
-          start: "top 20rem",
-          end: "+=100%"
-        }
-      });
-      tl.from(".about_side-text > .word", {
-        duration: 1,
-        opacity: 0.1,
-        stagger: 0.4,
-        ease: "power2.inOut"
-      });
+    new SplitType(".about_side-text", {
+      types: "words, chars",
+      tagName: "span"
+    });
+    const tl = gsapWithCSS.timeline({
+      scrollTrigger: {
+        trigger: ".about_side-text-wrapper",
+        scrub: true,
+        start: "top center",
+        end: "+=100%"
+      }
+    });
+    tl.from(".about_side-text > .word", {
+      duration: 1,
+      opacity: 0.1,
+      stagger: 0.4,
+      ease: "power2.inOut"
     });
   });
 })();
--- src/about.ts
+++ src/about.ts
@@ -1,30 +1,28 @@
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
-gsap.registerPlugin(SplitText);
 gsap.registerPlugin(ScrollTrigger);
 
 window.Webflow = window.Webflow || [];
 window.Webflow.push(() => {
-  document.addEventListener('DOMContentLoaded', function () {
-    new SplitText('.about_side-text', {
-      type: 'words, chars',
-      tagName: 'span',
-    });
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.about_side-text-wrapper',
-        scrub: true,
-        pin: true,
-        start: 'top 20rem',
-        end: '+=100%',
-      },
-    });
-    tl.from('.about_side-text > .word', {
-      duration: 1,
-      opacity: 0.1,
-      stagger: 0.4,
-      ease: 'power2.inOut',
-    });
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  new SplitType('.about_side-text', {
+    types: 'words, chars',
+    tagName: 'span',
+  });
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.about_side-text-wrapper',
+      scrub: true,
+      start: 'top center',
+      end: '+=100%',
+    },
+  });
+  tl.from('.about_side-text > .word', {
+    duration: 1,
+    opacity: 0.1,
+    stagger: 0.4,
+    ease: 'power2.inOut',
   });
 });
